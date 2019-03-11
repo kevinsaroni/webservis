9e65bf6e495db2b28c2ab52250256c662bebb543
siapa? <dimasahmadrizal@gmail.com>
2019-03-11 13:39:26 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    echo"<h1>Hello World</h1> <br/>";
+    
+    $text = "<table>
+    <tr>
+        <td>NIM</td>
+        <td> : </td>
+        <td>16.01.53.0064</td>
+    </tr>
+    <tr>
+        <td>Nama</td>
+        <td> : </td>
+        <td>Dimas Ahmad Rizal</td>
+    </tr>
+</table>";
+
+    echo $text;
+    
+?>
