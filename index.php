43a64a6e12a2ea1289d9d3073fc614bdbdad61a5
kevinsaroni <48478174+kevinsaroni@users.noreply.github.com>
2019-03-12 21:15:22 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,20 +1,15 @@
-<?php
-    echo"<h1>Hello World</h1> <br/>";
-    
-    $text = "<table>
-    <tr>
-        <td>NIM</td>
-        <td> : </td>
-        <td>16.01.53.0064</td>
-    </tr>
-    <tr>
-        <td>Nama</td>
-        <td> : </td>
-        <td>Dimas Ahmad Rizal</td>
-    </tr>
-</table> <br>";
 
-    echo $text;
-?>
+<h1>Hello World</h1><br/><table>
+	<tr>
+	<td>NIM</td>
+	<td> : </td>
+	<td>16.01.53.0162</td>
+	</tr>
+	<tr>
+	<td>Nama</td>
+	<td> : </td>
+	<td>Kevin Saroni</td>
+	</tr>
+	</table>
+	<ahref="/unisbank/unisbank">Go To Akademik</a>
 
-    <a href="/akademik">Go To Akademik</a>
